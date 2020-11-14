7736e2bb32a245467613adb561e0d376c3afcafe
unknown <1273163614@qq.com>
2020-11-14 18:01:41 +0800

    Swagger2接口文档工具的使用

--- soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
@@ -1,13 +1,26 @@
 package com.soft1851.api.controller.user;
 
+import com.soft1851.result.GraceResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Api(value = "用户信息相关Controller",tags = {"用户信息相关Controller"})
+@RequestMapping("user")
 public interface UserControllerApi {
 
     /**
      * 获取所有用户
      * @return users: 所有用户信息
      */
+    @ApiOperation(value = "获取所有用户信息",notes = "获取所有用户信息",httpMethod = "POST")
     @GetMapping("/users")
-    Object getUsers();
+    GraceResult getAllUsers();
+
+    @ApiOperation(value="获得用户基本信息",notes = "获得用户基本信息",httpMethod = "POST")
+    @PostMapping("/userInfo")
+    GraceResult getUserInfo(@RequestParam String userId);
 }
