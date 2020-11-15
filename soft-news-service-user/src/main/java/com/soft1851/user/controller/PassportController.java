5cfc89c015766dbf0b5044d33877e65235654250
unknown <1273163614@qq.com>
2020-11-15 21:01:25 +0800

    完善发送短信接口

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/PassportController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/PassportController.java
@@ -1,7 +1,9 @@
 package com.soft1851.user.controller;
 
+import com.soft1851.api.BaseController;
 import com.soft1851.api.controller.user.PassportControllerApi;
 import com.soft1851.result.GraceResult;
+import com.soft1851.utils.IpUtil;
 import com.soft1851.utils.SmsUtil;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +11,17 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-public class PassportController implements PassportControllerApi {
+public class PassportController extends BaseController implements PassportControllerApi {
     @Resource
     private SmsUtil smsUtil;
 
     @Override
     public GraceResult getCode(String mobile, HttpServletRequest request) {
+        String userIp = IpUtil.getRequestIp(request);
+        redis.setnx60s(MOBILE_SMSCODE+":"+userIp, userIp);
         String random = (int)((Math.random()*9+1)*10000)+"";
-        System.out.println(random);
         smsUtil.sendSms(mobile,random);
+        redis.set(MOBILE_SMSCODE+":"+mobile,random,30*60);
         return GraceResult.ok();
     }
 }
