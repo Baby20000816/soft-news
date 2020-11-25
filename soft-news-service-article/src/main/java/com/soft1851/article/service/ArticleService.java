0b72d4b7d053f1a1e74e91b64ea85dcd5a048768
unknown <1273163614@qq.com>
2020-11-25 10:00:48 +0800

    定时发布文章

--- soft-news-service-article/src/main/java/com/soft1851/article/service/ArticleService.java
+++ soft-news-service-article/src/main/java/com/soft1851/article/service/ArticleService.java
@@ -5,4 +5,8 @@ import com.soft1851.pojo.bo.NewArticleBO;
 
 public interface ArticleService {
     void createArticle(NewArticleBO newArticleBO, Category category);
+
+    void updateArticleStatus(String articleId,Integer pendingStatus);
+
+    void updateAppointToPublish();
 }
---
+++ soft-news-service-article/src/main/java/com/soft1851/article/task/TaskPublishArticle.java
@@ -0,0 +1,23 @@
+package com.soft1851.article.task;
+
+import com.soft1851.article.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TaskPublishArticle {
+    private final ArticleService articleService;
+    @Scheduled(cron = "0/10 * * * * ?")
+    private void publishArticles(){
+        System.out.println("执行定时任务:"+ LocalDateTime.now());;
+        articleService.updateAppointToPublish();
+    }
+}
