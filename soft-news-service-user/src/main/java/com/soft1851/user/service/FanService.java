b159c6cebfbcc99cda466497aa4d4d956057ac75
unknown <1273163614@qq.com>
2020-11-26 13:59:29 +0800

    关注

--- soft-news-service-user/src/main/java/com/soft1851/user/service/FanService.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/service/FanService.java
@@ -4,4 +4,8 @@ import com.soft1851.pojo.Fans;
 
 public interface FanService {
     Fans getFans(String writerId);
+
+    boolean isMeFollowThisWriter(String writerId, String fanId);
+
+    public void follow(String writerId,String fanId);
 }
